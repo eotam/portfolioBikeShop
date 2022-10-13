34f3a72c061a48b068144807f2c0c051ffacf0b5
Eotam <87521962+eotam@users.noreply.github.com>
2022-10-13 21:43:30 +0900

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,17 @@
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    loop: false,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide:true
+    },
+  });
+  
